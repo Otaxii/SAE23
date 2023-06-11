bfecbf05450711ead699a6e8f58ed5d252a270b4
DerekMICELI <133367800+DerekMICELI@users.noreply.github.com>
2023-06-11 18:55:28 +0200

    Create B201.php

---
+++ B201.php
@@ -0,0 +1,20 @@
+#!/opt/lampp/bin/php
+<?php
+   	shell_exec('mosquitto_sub -h mqtt.iut-blagnac.fr -t Student/by-room/B201/data -C 1 | jq ".[0].temperature, .[1].deviceName, .[1].devEUI, .[1].room, .[1].Building" > /home/sae23miceli/Desktop/stockage_donnees/B201.txt');
+    //$id_bd=mysqli_connect("localhost", "miceli", "22211833", "sae23") or die('Connexion impossible');
+	
+	$bat=shell_exec('more /home/sae23miceli/Desktop/stockage_donnees/B201.txt | sed -n 5p | tr -d \'"\'');
+	$room=shell_exec('more /home/sae23miceli/Desktop/stockage_donnees/B201.txt | sed -n 4p | tr -d \'"\'');
+	$code=shell_exec('more /home/sae23miceli/Desktop/stockage_donnees/B201.txt | sed -n 3p | tr -d \'"\'');
+	$nom=shell_exec('more /home/sae23miceli/Desktop/stockage_donnees/B201.txt | sed -n 2p | tr -d \'"\'');
+    $valeur=shell_exec('more /home/sae23miceli/Desktop/stockage_donnees/B201.txt | sed -n 1p');
+    
+    echo $valeur;
+    echo $nom;
+    echo $code;
+    echo $room;
+	echo $bat;
+   
+    //$requete="INSERT INTO `Mesure` (`Valeur`, `CodeCapt`) VALUES ($temp,1)";
+    //mysqli_query($id_bd,$requete) or die ("Ajout échoué $requete");
+?>
