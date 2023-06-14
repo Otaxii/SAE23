d5fe1f1eef0dcdcd9e206278b1101cb7a74666fe
DerekMICELI <133367800+DerekMICELI@users.noreply.github.com>
2023-06-14 21:32:31 +0200

    Update B203.php

--- MQTT/B203.php
+++ MQTT/B203.php
@@ -4,21 +4,21 @@
 {
 	//data recovery via MQTT broker and storage in a file
    	shell_exec('mosquitto_sub -h mqtt.iut-blagnac.fr -t Student/by-room/B203/data -C 1 | jq ".[0].temperature, .[1].deviceName, .[1].devEUI, .[1].room, .[1].Building" > /home/ndupont/Desktop/stockage_donnees/B203.txt');
-        $id_bd=mysqli_connect("localhost", "DUPONT", "22209481", "sae23") or die('Connexion impossible');
+        $id_bd=mysqli_connect("localhost", "DUPONT", "22209481", "sae23") or die('Connexion impossible'); //connexion to the data base
 	
-	$bat=shell_exec('more /home/ndupont/Desktop/stockage_donnees/B203.txt | sed -n 5p | tr -d \'"\'');
-	$room=shell_exec('more /home/ndupont/Desktop/stockage_donnees/B203.txt | sed -n 4p | tr -d \'"\'');
-	$code=shell_exec('more /home/ndupont/Desktop/stockage_donnees/B203.txt | sed -n 3p | tr -d \'"\'');
-	$nom=shell_exec('more /home/ndupont/Desktop/stockage_donnees/B203.txt | sed -n 2p | tr -d \'"\'');
-    $valeur=shell_exec('more /home/ndupont/Desktop/stockage_donnees/B203.txt | sed -n 1p');
+	$bat=shell_exec('more /home/ndupont/Desktop/stockage_donnees/B203.txt | sed -n 5p | tr -d \'"\''); //recovery and line filtering of data stored in the file
+	$room=shell_exec('more /home/ndupont/Desktop/stockage_donnees/B203.txt | sed -n 4p | tr -d \'"\''); //recovery and line filtering of data stored in the file
+	$code=shell_exec('more /home/ndupont/Desktop/stockage_donnees/B203.txt | sed -n 3p | tr -d \'"\''); //recovery and line filtering of data stored in the file
+	$nom=shell_exec('more /home/ndupont/Desktop/stockage_donnees/B203.txt | sed -n 2p | tr -d \'"\''); //recovery and line filtering of data stored in the file
+        $valeur=shell_exec('more /home/ndupont/Desktop/stockage_donnees/B203.txt | sed -n 1p'); //recovery and line filtering of data stored in the file
 	$valeurMDP="passgim";
 	$valeurLogin="batgim";
 	$CodeBat="2";
     
-    echo $valeur;
-    echo $nom;
-    echo $code;
-    echo $room;
+        echo $valeur;
+        echo $nom;
+        echo $code;
+        echo $room;
 	echo $bat;
    
 	$requetebat="INSERT INTO `Batiment` (`CodeBat`, `Nom`, `Login`, `MDP`) VALUES ('2','$bat','$valeurLogin','$valeurMDP')";
